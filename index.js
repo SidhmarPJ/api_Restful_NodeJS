55a30583af2949fa83c6cafb05ae58990ca7936e
SidhmarPJ <SidhmarP.JS@outlook.com>
2025-07-30 18:50:42 -0300

    status 201 metódo create adicionado

--- index.js
+++ index.js
@@ -2,23 +2,34 @@ const express = require("express");
 
 const server = express();
 
-server.get("/hello", (req, res) => {
-  const { nome, idade } = req.query;
-  return res.json({
-    title: "Hello World",
-    message: `Olá ${nome}, tudo bem?`,
-    idade: idade,
-  });
+server.use(express.json());
+
+let customers = [
+  { id: 1, name: "Dev Samurai", site: "http://devsamurai.com.br" },
+  { id: 2, name: "Google", site: "http://google.com" },
+  { id: 3, name: "UOL", site: "http://uol.com.br" },
+];
+
+server.get("/customers", (req, res) => {
+  return res.json(customers);
 });
 
-server.get("/hello/:nome/:idade", (req, res) => {
-  const { nome, idade } = req.params;
+server.get("/customers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const customer = customers.find((item) => item.id === id);
+  const status = customer ? 200 : 404;
 
-  return res.json({
-    title: "Hello World",
-    message: `Olá ${nome}, tudo bem?`,
-    idade: idade,
-  });
+  return res.status(status).json(customer);
+});
+
+server.post("/customers", (req, res) => {
+  const { name, site } = req.body;
+  const nextId = customers[customers.length - 1].id + 1;
+  const newCustomer = { id: nextId, name, site };
+
+  customers.push(newCustomer);
+
+  return res.status(201).json(newCustomer);
 });
 
 server.listen(3000, () => {
